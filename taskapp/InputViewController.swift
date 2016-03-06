d3f0920496a6ef47a0a0a4544a79dc89e67f877c
akikohatayama <akikohatayama@gmail.com>
2016-03-06 15:06:28 +0900

    task app

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,81 @@
+import UIKit
+import RealmSwift
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var category: UITextField!
+    
+    let realm = try! Realm()
+    var task:Task!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:"dismissKeyboard")
+        self.view.addGestureRecognizer(tapGesture)
+        
+        //枠線
+        contentsTextView.layer.borderWidth = 1
+        contentsTextView.layer.borderColor = UIColor(red:50/255.0, green:100/255.0, blue:50/255.0, alpha: 1.0).CGColor
+        contentsTextView.layer.cornerRadius = 8
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+        category.text = task.category
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        print("viewWillDisappear")
+        
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.task.category = self.category.text!
+            self.realm.add(self.task, update: true)
+        }
+        
+        setNotification(task)
+        super.viewWillDisappear(animated)
+    }
+    
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+    
+    // タスクのローカル通知を設定
+    func setNotification(task: Task) {
+        
+        // すでに同じタスクが登録されていたらキャンセル
+        for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
+            if notification.userInfo!["id"] as! Int == task.id {
+                UIApplication.sharedApplication().cancelLocalNotification(notification)
+                break   // breakに来るとforループから抜け出す
+            }
+        }
+        
+        let notification = UILocalNotification()
+        
+        notification.fireDate = task.date
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        notification.alertBody = "\(task.title)"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["id":task.id]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+    }
+    
+
+    
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,25 +1,163 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by akikohatayama on 2016/02/28.
-//  Copyright © 2016年 AkikoHatayama. All rights reserved.
-//
-
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //Realmインスタンス取得
+    let realm = try! Realm()
+    var task:Task!
+    
+    //DB内のタスクが格納されるリスト　日付未来順ソート：降順
+    let taskArray = try! Realm().objects(Task).sorted("date", ascending: false)
+    
+    //検索カテゴリー表示用リスト　日付未来順ソート：降順
+    var categoryArray = try! Realm().objects(Task).sorted("date", ascending: false)
+    
+    //検索カテゴリー表示用フラグ
+    var categoryFlg: Int = 0
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 検索バー初期表示文設定
+        searchBar.placeholder = "ここに入力してください"
+        searchBar.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    //サーチバーが更新されたとき
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        categoryFlg = 0
+        tableView.reloadData()
+    }
+    
+    //サーチバーがキャンセルされたとき
+    //func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+    //}
+    
+    //サーチバーで検索をしたとき
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
 
+        // 文字列で検索、日付未来順ソート：降順
+        //let filterString = String("category = '\(searchBar.text!)'")
+        categoryArray = realm.objects(Task).filter("category = '\(searchBar.text!)'").sorted("date", ascending: false)
 
+        categoryFlg = 1
+        dismissKeyboard()
+        tableView.reloadData()
+    }
+    
+
+    //tableView①　初期表示、再読み込みの際必ず処理される　データ数（セル数）返却
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(categoryFlg == 1){
+            return categoryArray.count
+        } else {
+           return taskArray.count
+        }
+    }
+    
+    //tableView②　初期表示、再読み込みの際必ず処理される　データ内容返却　データ数と処理回数が同じ
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //再利用可能なcell取得
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        var task: AnyObject?
+        
+        //検索カテゴリー表示
+        if(categoryFlg == 1){
+            task = categoryArray[indexPath.row]
+        } else {
+            task = taskArray[indexPath.row]
+        }
+
+        cell.textLabel?.text = task!.title
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString: String = formatter.stringFromDate((task?.date)!)
+        cell.detailTextLabel?.text = dateString
+        return cell
+    }
+    
+    //tableView③ 各セルを選択したとき
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("cellSegue",sender: nil)
+    }
+    
+    //セルが削除が可能なことを伝える
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    
+    //Deleteボタンが押されたとき
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            //ローカル通知をキャンセルする
+            let task = taskArray[indexPath.row]
+            
+            for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
+                if notification.userInfo!["id"] as! Int == task.id {
+                    UIApplication.sharedApplication().cancelLocalNotification(notification)
+                    break
+                }
+            }
+            
+            //データベースから削除する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+            }
+        }
+    }
+    
+    //画面遷移するとき
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        let inputViewController:InputViewController = segue.destinationViewController as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            task.date = NSDate()
+            
+            if taskArray.count != 0 {
+                task.id = taskArray.max("id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    
+    //入力画面から戻ってきたときにTableViewを更新
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
